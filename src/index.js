375e514cdd628fac30d97e4141edba7e7a3f28b9
SiljeT81 <siljebea@gmail.com>
2024-12-01 01:40:36 +0100

    Added search engine. No API

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", changeCityName);
+
+function changeCityName(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-city");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
