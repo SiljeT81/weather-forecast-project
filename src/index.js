7bafca35a334ad89d3d8d9871b09685a9910709a
SiljeT81 <siljebea@gmail.com>
2024-12-23 00:30:49 +0100

    Added icon data

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ function updateWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  
+  
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -15,6 +18,7 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon"/>`;
 }
 
 function formatDate(date) {
@@ -31,6 +35,10 @@ function formatDate(date) {
   ];
 
   let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
