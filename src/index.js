1e9e8b18a749b2686a39e899e4549ae7f70f0b88
SiljeT81 <siljebea@gmail.com>
2024-12-28 15:57:50 +0100

    Injected correct day info to forecast

--- src/index.js
+++ src/index.js
@@ -60,6 +60,14 @@ function changeCityName(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date (timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+  return days[date.getDay()];
+  
+}
+
 function getForecast(city) {
   let apiKey = "ffa664a53tdd3f1cb07eo89b459ff47a";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}}&key=${apiKey}&units=metric`;
@@ -79,7 +87,7 @@ function displayForecast(response) {
       forecastHtml +
       `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Tue</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
     <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
